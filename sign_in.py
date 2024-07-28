9ea3b3e1ae96239baf5c41aba5471799a36aa3f2
Peadar Grant <peadargrant@gmail.com>
2024-07-29 00:15:59 +0100

    sign in stub script

---
+++ sign_in.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Sign-in script for in-class attendance tracking in automation module
+# Peadar Grant
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+args = parser.parse_args()
+
