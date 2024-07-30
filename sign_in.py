6e311a9947f64479bcbde7a7f8737940661e4610
Peadar Grant <peadargrant@gmail.com>
2024-07-30 23:37:07 +0100

    sign in script

--- sign_in.py
+++ sign_in.py
@@ -7,5 +7,17 @@ from argparse import ArgumentParser
 parser = ArgumentParser()
 args = parser.parse_args()
 
-print("automated attendance tracking not working yet")
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+print(f'{bcolors.FAIL}automated attendance tracking not working yet{bcolors.ENDC}')
 
